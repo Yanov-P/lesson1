297cbca8293468f4e80b776dea95607fdcb590a7
Yanov-P <yanovpropeshtinktovich@gmail.com>
2019-09-20 17:50:07 +0300

    вывод таблицы оформлен в виде функции

--- table.php
+++ table.php
@@ -1,62 +1,57 @@
 <?php
-function square($x){
-    return $x*$x;
+function show_simple_numbers_table($maxnum = 1000, $maxinrow = 20){
+print<<<TABLE
+Таблица простых чисел до $maxnum по $maxinrow в строку
+<table border="1" align="center" >
+<tr>
+TABLE;
+//amount of found simple numbers
+    $prostCount = 0;
+//a number which simplicity is checking
+    $num = 2;
+    while ($prostCount < $maxnum){
+        //true if simple number found
+        $found = true;
+        for($n = 2; $n < sqrt($num); $n++){
+            if($num%$n == 0){
+                //not a simple
+                $found = false;
+                break;
+            }
+        }
+        //printing to table if simple
+        if($found == true){
+            $prostCount++;
+            print("<td>");
+            print"$num";
+            print("</td>");
+            //start new row if end is reached
+            if($prostCount%$maxinrow == 0){
+                print("</tr>");
+                print("<tr>");
+            }
+        }
+        $num++;
+    }
+
+    print<<<ENDTABLE
+</tr>
+</table>
+ENDTABLE;
+return;
 }
+
 print <<<TITLE
 <html>
 <head>
 <title>Таблица простых чисел</title>
 </head>
 <body>
-Таблица простых чисел до 
-<table border="1" align="center" >
-<tr>
 TITLE;
 
-$maxnum = 1000;
-$maxinrow = 20;
-print("$maxnum по $maxinrow в строку");
-
-$prostCount = 0;
-$rowpos=0;
-$num = 2;
-while ($prostCount < $maxnum){
-    $found = true;
-    for($n = 2; $n < sqrt($num); $n++){
-     if($num%$n == 0){
-         $found = false;
-         break;
-     }
-    }
-    if($found == true){
-        $prostCount++;
-
-        print("<td>");
-        print"$num";
-        print("</td>");
-        if($prostCount%$maxinrow == 0){
-            print("</tr>");
-            print("<tr>");
-        }
-    }
-    $num++;
-}
-
-//$sz = 15;
-//for($i = 0; $i < $sz; $i++){
-//    print("<tr>");
-//    for($j = 0; $j < $sz; $j++){
-//        print("<td>");
-//        print($i*$sz + $j);
-//        print("</td>");
-//    }
-//    print("</tr>");
-//}
-
+show_simple_numbers_table(500,13);
 
 print <<<FOOT
-</tr>
-</table>
 </body>
 </html>
 FOOT;
